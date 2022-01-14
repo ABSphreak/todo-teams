33da2c8007fe9899252cf5a68d2374a24460c045
ABSphreak <absphreak@outlook.com>
2022-01-14 19:05:16 +0530

    ➕ Add Initial Data

---
+++ src/data/mock.js
@@ -0,0 +1,32 @@
+export const todos = [
+	{
+		id: 1,
+		text: 'Learn React',
+		assignedTo: [1],
+		completed: false,
+	},
+];
+
+export const teams = [
+	{
+		id: 1,
+		name: 'Team 1',
+		members: [
+			{
+				id: 1,
+				name: 'Member 1',
+				todosAssigned: [],
+			},
+			{
+				id: 2,
+				name: 'Member 2',
+				todosAssigned: [],
+			},
+			{
+				id: 3,
+				name: 'Member 3',
+				todosAssigned: [],
+			},
+		],
+	},
+];
